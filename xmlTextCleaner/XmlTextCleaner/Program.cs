99fbd35ad4f94c9f54af52b40426c2f6c4a2ed78
Aleksandr Gnatiuk <Aleksandr_Gnatiuk@epam.com>
2018-10-26 15:38:21 +0300

    Project was added

---
+++ xmlTextCleaner/XmlTextCleaner/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace XmlTextCleaner
+{
+    class Program
+    {
+        public static class ResxReader
+        {
+            public static XDocument RemoveCertainNodes(string resxFile, string searchLine)
+            {   var xdoc = XDocument.Load(resxFile);
+                xdoc.XPathSelectElements($"root/data[contains(@name, '{searchLine}')]")?
+                    .Remove(); 
+
+                return xdoc;
+            }
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var result = ResxReader.RemoveCertainNodes(args[0], args[1]);
+                result.Save("result.resx");
+            }   
+        }
+    }
+}
